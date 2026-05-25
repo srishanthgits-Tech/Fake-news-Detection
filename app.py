e43fbdc2f02e27e6812074bc2fffaa86efa02540
srishanth <srishanthsrigadde007@gmail.com>
2026-05-25 14:27:00 +0530

    updated render config

--- app.py
+++ app.py
@@ -299,7 +299,11 @@ def logout():
     return redirect(url_for('login'))
 
 # Run Flask App
+import os
+
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all()  # Ensure tables are created before running
-    app.run(debug=True)
+        db.create_all()
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
